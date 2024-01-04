4bfaed54b30646a9a80f739a6d3b4d2a4c08370a
GuanyuChan <Hugo.Chan1@outlook.com>
2024-01-04 21:35:35 +0800

    Update Background.js

--- Background.js
+++ Background.js
@@ -5,17 +5,3 @@ chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
     chrome.tabs.executeScript(tabId, { file: 'content.js' });
   }
 });
-
-
-// content.js
-
-window.addEventListener('spfdone', skipAdsAndContinue);
-
-window.addEventListener('yt-navigate-finish', skipAdsAndContinue);
-
-function skipAdsAndContinue() {
-  const skipButton = document.querySelector('.ytp-ad-skip-button');
-  if (skipButton) {
-    skipButton.click();
-  }
-}
