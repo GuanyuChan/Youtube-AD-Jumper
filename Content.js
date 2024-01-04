4fbd2410215e7c72285b9816941fa495844ffd4e
GuanyuChan <Hugo.Chan1@outlook.com>
2024-01-04 21:36:02 +0800

    Update Content.js

--- Content.js
+++ Content.js
@@ -1,12 +1,3 @@
-// background.js
-
-chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
-  if (changeInfo.status === 'complete' && tab.url.includes('youtube.com/watch')) {
-    chrome.tabs.executeScript(tabId, { file: 'content.js' });
-  }
-});
-
-
 // content.js
 
 window.addEventListener('spfdone', skipAdsAndContinue);
